fb906be79795cf2bbdeea4e202e43f0e9435b9fc
limnyn <hogandi001@gmail.com>
2023-01-03 23:05:45 +0900

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0P205Fenfh0GTpZ4Cse1Vmxi55VJ4UNM",
+  authDomain: "nwitter-8169c.firebaseapp.com",
+  projectId: "nwitter-8169c",
+  storageBucket: "nwitter-8169c.appspot.com",
+  messagingSenderId: "1015216595206",
+  appId: "1:1015216595206:web:efda7efb037e62fdf2fe87",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
