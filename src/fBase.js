46fb9c41281402b70d99071e8daf22e82daf5f3c
limnyn <hogandi001@gmail.com>
2023-01-06 13:59:03 +0900

    add authservicde

--- src/fBase.js
+++ src/fBase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,4 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+export const authService = getAuth(app);
+// export const authService = getAuth();
