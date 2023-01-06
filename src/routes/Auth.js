d17084182a223ffdd15e9156c6f06a19f8b9583d
limnyn <hogandi001@gmail.com>
2023-01-06 14:41:29 +0900

    add Auth onchange

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        ></input>
+        <input type="submit" value="Log In"></input>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with gitHub</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
